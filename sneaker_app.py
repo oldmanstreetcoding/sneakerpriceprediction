500a747c60b9d3713731a1b4443fdeb738a22b91
Street Coding <oldmanstreetcoding@gmail.com>
2024-03-16 04:33:20 -0400

    edit title

--- sneaker_app.py
+++ sneaker_app.py
@@ -13,7 +13,7 @@ dfcolor = dfcolor.apply(proper_case)
 def main():
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-        <h1 style="color:white;text-align:center;">Sneaker Resale Value Predictori</h1>
+        <h1 style="color:white;text-align:center;">Sneaker Resale Value Predictor</h1>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
