afe72db5c604ba039443d1e91d393dd4808ee25f
Street Coding <64743844+oldmanstreetcoding@users.noreply.github.com>
2024-03-16 19:14:49 +0000

    edit ReadMe

--- main.py
+++ main.py
@@ -22,48 +22,18 @@ with ui.card().classes('desktop').style('margin:auto;margin-top:10px;'):
     with ui.row().classes('justify-center w-full').style('font-weight: bold;font-size:30px;background-color:tomato;color:white;padding:10px;'):
         ui.html('Resale Value Prediction')
     with ui.row().classes('w-full').style('border: 2px solid red; padding: 15px;'):
-
-        # with ui.tabs().classes('w-full') as tabs:
-        #     one = ui.tab('Form')
-        #     # two = ui.tab('Result')
-        # with ui.tab_panels(tabs, value=one).classes('w-full'):
-        #     with ui.tab_panel(one):
-        #         ui.html('<p>Hi, This is a Resale Value Calculator for Newly Released Sneakers ! Please give us a minute to input some parameters, then we will give you a value of the sneaker you would like to buy.<p>')
-        
-        #         txbrand = ui.select(['Nike', 'Adidas', 'Jordan'], label='Brand').style('width: 100%;')
-        #         txcolor = ui.select(['Black'], label='Color').style('width: 100%;')
-        #         txmodel = ui.input(label='Model', 
-        #                 placeholder='Type your Model...', 
-        #                 validation={'Input too long': lambda value: len(value) < 20}).style('width: 100%;')
-        #         txgender = ui.select(['Man', 'Woman'], label='Gender').style('width: 100%;')
-        #         txsize = ui.number(label='US Size', 
-        #                         value=5, 
-        #                         format='%.2f').style('width: 100%;')
-
-        #         with ui.dialog() as dialog, ui.card():
-        #             with ui.row().classes('w-full').style('padding:5px;'):
-        #                 ui.html('Prediction Result').style('font-size:30px')
-        #                 ui.html('Here is the result of prediction based on the data you provided')
-        #                 ui.html('$ XXX').style('font-size:50px').style('text-decoration: underline')
-        #                 ui.html('Does this help? if you would like to continue, please push the button below')
-        #                 ui.button('Back', on_click=dialog.close)
-                
-        #         with ui.row().classes('w-full'):
-        #             ui.button('Predict', on_click=dialog.open)
-            # with ui.tab_panel(two):
-            #     ui.label('Second tab')
                 
         ui.html('<p>Hi, This is a Resale Value Calculator for Newly Released Sneakers ! Please give us a minute to input some parameters, then we will give you a value of the sneaker you would like to buy.<p>')
         
         txbrand = ui.select(['Nike', 'Adidas', 'Jordan'], label='Brand').style('width: 100%;')
-        txcolor = ui.select(['Black'], label='Color').style('width: 100%;')
+        txcolor = ui.select(['Black', 'White', 'Red', 'Blue', 'Yellow'], label='Color').style('width: 100%;')
         txmodel = ui.input(label='Model', 
                 placeholder='Type your Model...', 
                 validation={'Input too long': lambda value: len(value) < 20}).style('width: 100%;')
         txgender = ui.select(['Man', 'Woman'], label='Gender').style('width: 100%;')
         txsize = ui.number(label='US Size', 
                            value=5, 
-                           format='%.2f').style('width: 100%;')
+                           format='%.1f').style('width: 100%;')
 
         with ui.dialog() as dialog, ui.card():
             with ui.row().classes('w-full').style('padding:5px;'):
